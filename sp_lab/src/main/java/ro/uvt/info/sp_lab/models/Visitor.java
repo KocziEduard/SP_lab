3c02a637c8fc31aeac199b3cffb12849dadfc716
Eoczi <106028382+Eoczi@users.noreply.github.com>
2024-01-08 23:54:58 +0200

    book service and controller (rest api)

--- sp_lab/src/main/java/ro/uvt/info/sp_lab/models/Visitor.java
+++ sp_lab/src/main/java/ro/uvt/info/sp_lab/models/Visitor.java
@@ -3,6 +3,7 @@ package ro.uvt.info.sp_lab.models;
 public interface Visitor {
     void visitParagraph(Paragraph p);
     void visitSection(Section s);
+    void visitBook(Book b);
     void visitImage(Image i);
     void visitTable(Table t);
 }
